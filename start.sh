58d3f63d09cadb62607e9ca3f115f4cfa5635da5
Décius <72174645+deciusmotta@users.noreply.github.com>
2025-10-12 19:58:13 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Instalando dependências (sem build de wheel)..."
-pip install --no-build-isolation --prefer-binary -r requirements.txt
-
-echo "Iniciando aplicação Flask..."
-python app.py
